d07db1d16efa4b5cf1cce8473b54a4d86234f612
Ekaterina Folkina <folkina7@gmail.com>
2023-04-08 18:19:04 +0300

    transactions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
 import { FriendList } from 'components/FriendList/FriendList';
+import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
 import user from 'path/user.json';
 import data from 'path/data.json';
 import friends from 'path/friends.json';
+import transactions from 'path/transactions.json';
 
 export const App = () => {
   return (
@@ -26,6 +28,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
