f41e31af3ed85748bbef5c9c356bd12414327cb2
Ekaterina Folkina <folkina7@gmail.com>
2023-04-08 17:45:08 +0300

    FriendList

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
 import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
+import { FriendList } from 'components/FriendList/FriendList';
 import user from 'path/user.json';
 import data from 'path/data.json';
+import friends from 'path/friends.json';
 
 export const App = () => {
   return (
@@ -22,8 +24,8 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-
       <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
     </div>
   );
 };
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import css from './FriendList.module.css';
+import { FriendListItem } from 'components/FriendListItem/FriendListItem';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul classame={css.friendList}>
+      {friends.map(item => {
+        return (
+          <FriendListItem
+            key={item.id}
+            avatar={item.avatar}
+            name={item.name}
+            isOnline={item.isOnline}
+          />
+        );
+      })}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    })
+  ),
+};
